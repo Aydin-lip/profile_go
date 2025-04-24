6aee217ddec8db809b0af21690bd8e17ba3da954
Aydin <ms1166760@gmail.com>
2025-04-25 03:14:58 +0330

    Add cors origin

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
 	"userProfile/config"
 	"userProfile/internal/database"
 	"userProfile/internal/models"
@@ -26,6 +31,16 @@ func main() {
 	validation.SetupCustom()
 
 	r := routes.SetupRouter(db)
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	port := config.GetEnv("PORT", ":8080")
 	r.Run(port)
 }
